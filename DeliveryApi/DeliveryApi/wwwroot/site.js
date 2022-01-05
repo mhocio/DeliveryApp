49d56f2e2bdf74256fd30d01163040e1a86d33d7
Mikołaj <mikolajhoscilo@gmail.com>
2022-01-06 00:57:18 +0100

    Fix leaflet map
    https://stackoverflow.com/questions/64073635/blank-map-tiles-error-410-gone-mapbox-leaflet-js
    https://docs.mapbox.com/help/troubleshooting/migrate-legacy-static-tiles-api/#will-the-apis-return-an-error-when-classic-styles-are-no-longer-supported

--- DeliveryApi/DeliveryApi/wwwroot/site.js
+++ DeliveryApi/DeliveryApi/wwwroot/site.js
@@ -180,14 +180,14 @@ function loadMap() {
     }).setView([52.229, 21.011], 14);
 
     L.tileLayer(
-        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
         {
             maxZoom: 18,
             attribution:
                 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
                 'Imagery � <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: "mapbox.streets"
+            id: "mapbox/streets-v11"
         }
     ).addTo(mymap);
 
